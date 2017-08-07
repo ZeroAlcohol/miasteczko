1e7df4ff8508698021c695c879498efecad88631
Piotr Dyszkowski <piod94@gmail.com>
2017-08-07 22:43:34 +0200

    Add trivial player control

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,5 +1,6 @@
 #include"Player.hpp"
 #include <iostream>
+#include <cmath>
 
 Player::Player(std::string p_name) : m_name(p_name)
 {
@@ -26,8 +27,36 @@ std::string Player::getName()
 
 void Player::run()
 {
-   // std::cout<< "my name is "<< m_name <<std::endl;
+	// temporary 
 
+	double l_dirx{}, l_diry{};
+	double l_speed{ 2 };
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		l_dirx = -1;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		l_dirx = 1;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		l_diry = -1;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		l_diry = 1;
+	}
+
+	bool l_move = l_dirx || l_diry;
+
+	if (l_move)
+	{
+		double angle = std::atan2(l_diry, l_dirx);
+		m_sprite.move(std::cos(angle) * l_speed, std::sin(angle) * l_speed);
+	}
 }
 
 void Player::render(sf::RenderWindow& p_window)
