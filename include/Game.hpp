6edd845386903193dfb1231349bd46cdf17f07dc
Piotr Dyszkowski <piod94@gmail.com>
2017-08-15 19:01:47 +0200

    Move game objects into level struct

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,9 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
 #include <SFML/Graphics.hpp>
-#include "IObject.hpp"
-#include <memory>
-#include <list>
+#include <level.hpp>
 
 class Game
 {
@@ -15,11 +13,7 @@ public:
     bool createGame();
 
 private:
-
-	std::list<std::unique_ptr<IObject>> m_activeObjects; //objects which are performing actions during run()
-	std::list<std::unique_ptr<IObject>> m_passiveObjects; //objects which aren't performing actions during run()
-	
-
+	Level m_level;
 };
 
 
--- src/Game.cpp
+++ src/Game.cpp
@@ -15,18 +15,17 @@ Game::Game()
 
 bool Game::createGame()
 {
-    m_activeObjects.push_back(GameObjectFactory().createPlayer());
+    m_level.activeObjects.push_back(GameObjectFactory().createPlayer());
+    m_level.passiveObjects.push_back(GameObjectFactory().createPassiveRenderRectangle(200, 100, 100, 100, 50, { 255, 0, 0 }));
+    m_level.passiveObjects.push_back(GameObjectFactory().createPassiveRenderRectangle(800, 120, 100, 300, 75, { 0, 255, 0 }));
+    m_level.passiveObjects.push_back(GameObjectFactory().createPassiveRenderRectangle(700, 320, 150, 150, 0, { 0, 0, 255 }));
 
-    m_passiveObjects.push_back(GameObjectFactory().createPassiveRenderRectangle(200, 100, 100, 100, 50, { 255, 0, 0 }));
-    m_passiveObjects.push_back(GameObjectFactory().createPassiveRenderRectangle(800, 120, 100, 300, 75, { 0, 255, 0 }));
-    m_passiveObjects.push_back(GameObjectFactory().createPassiveRenderRectangle(700, 320, 150, 150, 0, { 0, 0, 255 }));
+    auto l_activeObjectsIterator = std::find_if(m_level.activeObjects.begin(), m_level.activeObjects.end(), [](const auto& p){return p == nullptr;});
+	if(m_level.activeObjects.end() != l_activeObjectsIterator && nullptr == *l_activeObjectsIterator)
+		return false;
 
-    auto l_activeObjectsIterator = std::find_if(m_passiveObjects.begin(), m_passiveObjects.end(), [](const auto& p){return p == nullptr;});
-    if(*l_activeObjectsIterator == nullptr)
-        return false;
-
-    auto l_passiveObjectsIterator = std::find_if(m_passiveObjects.begin(), m_passiveObjects.end(), [](const auto& p){return p == nullptr;});
-    if(*l_passiveObjectsIterator == nullptr)
+    auto l_passiveObjectsIterator = std::find_if(m_level.passiveObjects.begin(), m_level.passiveObjects.end(), [](const auto& p){return p == nullptr;});
+    if(m_level.passiveObjects.end() != l_passiveObjectsIterator && *l_passiveObjectsIterator == nullptr)
         return false;
 
     return true;
@@ -39,7 +38,7 @@ void Game::onEvent(sf::Event & p_event)
 
 void Game::update(const float dt)
 {
-	for (auto t = m_activeObjects.begin(); t != m_activeObjects.end(); t++)
+	for (auto t = m_level.activeObjects.begin(); t != m_level.activeObjects.end(); t++)
 	{
 		(*t)->run();
 	}
@@ -49,12 +48,12 @@ void Game::renderFrame(sf::RenderWindow & p_window, const float dt)
 {
 	// here render backround
 
-	for (auto& t : m_passiveObjects)
+	for (auto& t : m_level.passiveObjects)
 	{
         t->render(p_window);
 	}
 
-	for (auto& t : m_activeObjects)
+	for (auto& t : m_level.activeObjects)
 	{
 		t->render(p_window);
 	}
