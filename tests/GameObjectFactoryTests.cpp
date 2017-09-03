b0fb7003b09b280d82f3e9285720e62e490494aa
Piotr Dyszkowski <piod94@gmail.com>
2017-09-03 16:36:07 +0200

    Add GameObjectFactory tests

---
+++ tests/GameObjectFactoryTests.cpp
@@ -0,0 +1,35 @@
+#include "catch.hpp"
+#include "TextureContainer.hpp"
+#include "GameObjectFactory.hpp"
+#include "PassiveTexturedRectangle.hpp"
+#include "Player.hpp"
+
+TEST_CASE("should create suitable objects")
+{
+	TextureContainer::createResources();
+
+	SECTION("should create PassiveTexturedRectange")
+	{
+		LevelObjectData l_objectData
+		{ 
+			"flowerBox00", "passive-textured-rectangle", "idle", 50.0f, 50.0f, 0.0f 
+		};
+		const std::unique_ptr<IObject> l_passiveObject{ GameObjectFactory().createLevelObject(l_objectData) };	
+		auto l_rawPasiveObject{ dynamic_cast<const PassiveTexturedRectangle *> (l_passiveObject.get()) };
+
+		REQUIRE(nullptr != l_rawPasiveObject);
+	}
+
+	SECTION("should create Player")
+	{
+		LevelObjectData l_objectData
+		{ 
+			"player01", "player", "idle", 200.0f, 100.0f, 0.0f 
+		};
+
+		const std::unique_ptr<IObject> l_player{ GameObjectFactory().createLevelObject(l_objectData) };
+		auto l_rawPlayer{ dynamic_cast<const Player *> (l_player.get()) };
+
+		REQUIRE(nullptr != l_rawPlayer);
+	}
+}
